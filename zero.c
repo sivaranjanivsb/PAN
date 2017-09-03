dff1ee2d373ad0b4a490f007a3d96df070d91ac3
sivaranjanivsb <31504326+sivaranjanivsb@users.noreply.github.com>
2017-09-03 15:30:54 +0100

    Create zero.c

---
+++ zero.c
@@ -0,0 +1,19 @@
+# include<stdio.h>
+void main()
+{
+int a;
+printf("the given no is");
+scanf("%d",&a);
+if(a>0)
+{
+printf("no is positive");
+}
+elseif(a<0)
+{
+printf("no is negative");
+}
+else
+{
+printf("no is zero");
+}
+}
